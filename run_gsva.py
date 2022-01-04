51e8c4a6328a9a05cc938e5c01fbc011c9291ab5
Matthew Bernstein <matthewbernstein@Matthews-MacBook-Pro-2.local>
2022-01-04 10:58:24 -0600

    Added more comments. Input validation

--- run_gsva.py
+++ run_gsva.py
@@ -4,7 +4,6 @@
 
 from optparse import OptionParser
 import json
-import numpy as np
 import pandas as pd
 from rpy2.robjects import r, pandas2ri
 from rpy2.robjects.conversion import localconverter
@@ -12,17 +11,41 @@ import rpy2.robjects as ro
 import sys
 
 def run_GSVA(df_expr, gene_set_to_genes, distr='Gaussian'):
+    """
+    Parameters
+    ----------
+    df_expr : DataFrame
+        A gene-by-sample Pandas DataFrame storing the gene expression 
+        matrix.
+    gene_set_to_genes: dictionary
+        A dictionary mapping each gene set name (string) to list of gene
+        names (list of strings).
+    distr: string
+        The distribution to use in the GSVA algorithm. Must be either 
+        'Poisson' or 'Gaussian'.
+
+    Returns
+    -------
+    df_gsva: DataFrame
+        A gene set-by-sample dataframe storing the GSVA scores for each
+        gene set within each sample.
+    """
+    # Re-format gene-set information to make compatible with GSVA
     gene_set_names = []
     gene_lists = []
     for gene_set, genes in gene_set_to_genes.items():
         gene_set_names.append(gene_set)
         gene_lists.append(genes)
+
+    # Convert Python objects to rpy2 objects to pass to R
     with localconverter(ro.default_converter + pandas2ri.converter):
         r_expr = ro.conversion.py2rpy(df_expr)
         r_genes = ro.conversion.py2rpy(list(df_expr.index))
         r_gene_set_names = ro.conversion.py2rpy(gene_set_names)
         r_gene_lists = ro.conversion.py2rpy(gene_lists)
         r_gs = ro.vectors.ListVector(gene_set_to_genes)
+
+    # The R function for running GSVA
     rstring="""
         function(expr, genes, gs) {{
             library(GSVA)
@@ -35,12 +58,16 @@ def run_GSVA(df_expr, gene_set_to_genes, distr='Gaussian'):
             df
         }}
     """.format(distr)
+
+    # Run the R code
     r_func = ro.r(rstring)
     r_res = r_func(r_expr, r_genes, r_gs)
+
+    # Convert the R output  to Python objects
     with localconverter(ro.default_converter + pandas2ri.converter):
-        df_res = ro.conversion.rpy2py(r_res)
-    print(df_res)
-    return df_res
+        df_gsva = ro.conversion.rpy2py(r_res)
+
+    return df_gsva
 
 
 def _parse_gene_sets(gene_sets_f):
@@ -55,7 +82,7 @@ def _parse_gene_sets(gene_sets_f):
 
 
 def main():
-    usage = "" # TODO
+    usage = "python run_gsva.py <input>" # TODO
     parser = OptionParser(usage=usage)
     parser.add_option("-t", "--transpose", action="store_true", help="Take transpose of input")
     parser.add_option("-d", "--distribution", help="Distribution to use in GSVA {'Poisson' or 'Gaussian'}")
@@ -64,11 +91,13 @@ def main():
 
     data_f = args[0]
     gene_sets_f = args[1]
+
     if options.distribution is None:
         print("Warning! No distribution was specified (see the '--distribution' flag). Using 'Guassian' by default.")
         distr = 'Gaussian'
     else:
         distr = options.distribution
+        assert distr in {'Poisson', 'Gaussian'}, f"The `--distribution` argument must be either 'Poisson' or 'Gaussian'. '{distr}' is invalid."
     out_f = options.out_file
 
     gene_set_to_genes = _parse_gene_sets(gene_sets_f)
